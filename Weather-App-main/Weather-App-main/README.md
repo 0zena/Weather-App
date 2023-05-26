322b7b8005f9d8e0f247ee2d0a77995671d8e04f
zena <91028915+0zena@users.noreply.github.com>
2023-05-26 17:52:02 +0300

    5th commit
    almost done

---
+++ Weather-App-main/Weather-App-main/README.md
@@ -0,0 +1,9 @@
+<h1>Weather-App</h1>
+Windows forms weather app using <a href="https://openweathermap.org/">OpenWeatherMap</a> API
+
+<h3>HTTP request USAGE</h3>
+<ul>
+  <li>First API request used to show the Weather at the moment</li>
+  <li>Second API request for displaying Weather forecast to the second four days</li>
+</ul>
+<p>Forecast and current weather you can see by clicking the buttom, that will switch the panels.</p> 
---
+++ Weather-App-main/Weather-App-main/WeatherApp/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using IWshRuntimeLibrary;
+using System.Collections.Generic;
+using System.Globalization;
+
+
+namespace WeatherApp
+{
+    public partial class Form1 : Form
+    {
+        private string City;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            const string key = "154d1eeac867d0df4b0eba68a5638835";
+            if (string.IsNullOrEmpty(City)) { City = "Riga"; }
+
+            panel1.Visible = true;
+            panel2.Visible = false;
+
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            Shortcut(folder);
+
+            #region Current Weather API 
+            string url = $"https://api.openweathermap.org/data/2.5/weather?&q={City}&lang=en&appid={key}&units=metric";
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream());
+            string response = streamReader.ReadToEnd();
+
+            WeatherData weatherResponse = JsonConvert.DeserializeObject<WeatherData>(response);
+
+            var sunrise = DateTimeOffset.FromUnixTimeSeconds(weatherResponse.Sys.Sunrise);
+            var sunset = DateTimeOffset.FromUnixTimeSeconds(weatherResponse.Sys.Sunset);
+            #endregion
+
+            #region Forecast Weather API
+            string f_url = $"https://api.openweathermap.org/data/2.5/forecast?&q={City}&lang=en&appid={key}&units=metric";
+
+            HttpWebRequest httpWebRequest1 = (HttpWebRequest)WebRequest.Create(f_url);
+            HttpWebResponse httpWebResponse1 = (HttpWebResponse)httpWebRequest1.GetResponse();
+
+            StreamReader streamReader1 = new StreamReader(httpWebResponse1.GetResponseStream());
+            string response1 = streamReader1.ReadToEnd();
+
+            ForecastRoot forecastRoot = JsonConvert.DeserializeObject<ForecastRoot>(response1);
+            
+            List<Forecast> forecasts = forecastRoot.Forecasts;
+            #endregion
+
+            #region First Panel Labels
+            // Main data
+            label1.Text = WeatherData.Name + ", " + weatherResponse.Sys.Country;
+            label3.Text = Math.Round(weatherResponse.Main.Temperature) + "°C";
+            label5.Text = "Feels like: " + Math.Round(weatherResponse.Main.FeelsLikeTemperature) + "°C";
+            label6.Text = "Weather: " + WeatherData.WeatherInfo[0].Description;
+            label7.Text = "Wind speed: " + weatherResponse.Wind.Speed + " m/s";
+            label8.Text = "Humidity: " + weatherResponse.Main.Humidity;
+
+            // Details
+            label2.Text = "Today`s details";
+            label4.Text = "Sunrise: " + sunrise.LocalDateTime.ToString("HH:mm");
+            label9.Text = "Sunset: " + sunset.LocalDateTime.ToString("HH:mm");
+            label11.Text = "Max: " + Math.Round(weatherResponse.Main.MaxTemperature) + "°C";
+            label12.Text = "Min: " + Math.Round(weatherResponse.Main.MinTemperature) + "°C";
+
+            pictureBox1.Load($"http://openweathermap.org/img/wn/{WeatherData.WeatherInfo[0].Icons}@2x.png");
+
+            // Header Clock
+            label10.Text = DateTime.Now.ToString("HH:mm");
+            #endregion
+
+            #region Forecast data processing
+            try
+            {
+                List<string> ForecastList = new List<string>();
+                List<string> ForecastIconCode = new List<string>();
+
+                for (int i = 0; i < forecasts.Count; i++)
+                {
+                    if (i % 8 == 0)
+                    {
+                        string DatePath = DateTime.ParseExact(forecasts[i].DateText.Substring(0, 10), "yyyy-MM-dd", CultureInfo.InvariantCulture).ToString("dddd");
+                        string listItem = $"\t{DatePath}\nTemperature: {Math.Round(forecasts[i].Main.Temperature)}°C\nHumidity: {forecasts[i].Main.Humidity}\n";
+                        string iconCode = forecasts[i].Info[0].Icon;
+                        ForecastIconCode.Add(iconCode);
+                        ForecastList.Add(listItem);
+                    }
+                }
+                #region Second Panel Labels
+                // Temp, Humidity, Date
+                label13.Text = DateTime.Now.ToString("HH:mm");
+                label15.Text = ForecastList[1];
+                label16.Text = ForecastList[2];
+                label17.Text = ForecastList[3];
+                label18.Text = ForecastList[4];
+                // Icons
+                pictureBox2.Load($"http://openweathermap.org/img/wn/{ForecastIconCode[0]}@2x.png");
+                pictureBox3.Load($"http://openweathermap.org/img/wn/{ForecastIconCode[1]}@2x.png");
+                pictureBox4.Load($"http://openweathermap.org/img/wn/{ForecastIconCode[2]}@2x.png");
+                pictureBox5.Load($"http://openweathermap.org/img/wn/{ForecastIconCode[3]}@2x.png");
+                #endregion
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Error: {exception.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            #endregion
+        }
+
+        #region Button Controls
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = false;
+            panel1.Visible = true;
+        }
+        #endregion
+
+        private void Shortcut(string folder)
+        {
+            WshShell wshShell = new WshShell();
+            string fileName = folder + "\\" + ProductName + ".lnk";
+            IWshShortcut shortcut = (IWshShortcut)wshShell.CreateShortcut(fileName);
+            shortcut.TargetPath = Application.ExecutablePath;
+            shortcut.Save();
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            InitializeComponent();
+        }
+    }
+
+    #region Json Current
+    public class WeatherData 
+    {
+        public TemperatureData Main { get; set; }
+
+        [JsonProperty("name")]
+        public static string Name { get; set; }
+
+        public WeatherSysData Sys { get; set; }
+
+        [JsonProperty("weather")]
+        public static WeatherInfo[] WeatherInfo { get; set; }
+
+        [JsonProperty("wind")]
+        public WindInfo Wind { get; set; }
+    }
+
+    public class WindInfo
+    {
+        [JsonProperty("speed")]
+        public float Speed { get; set; }
+    }
+
+    public class WeatherSysData
+    {
+        [JsonProperty("sunrise")]
+        public int Sunrise { get; set; }
+
+        [JsonProperty("sunset")]
+        public int Sunset { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+    }
+
+    public class TemperatureData
+    {
+        [JsonProperty("feels_like")]
+        public float FeelsLikeTemperature { get; set; }
+
+        [JsonProperty("temp")]
+        public float Temperature { get; set; }
+
+        [JsonProperty("temp_max")]
+        public float MaxTemperature { get; set; }
+
+        [JsonProperty("temp_min")]
+        public float MinTemperature { get; set; }
+
+        [JsonProperty("humidity")]
+        public int Humidity { get; set; }
+    }
+
+    public class WeatherInfo
+    {
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        
+        [JsonProperty("icon")]
+        public string Icons { get; set; }
+    }
+    #endregion
+
+    #region Json Forecast
+    public class ForecastRoot
+    {
+        [JsonProperty("list")]
+        public List<Forecast> Forecasts { get; set; }
+    }
+
+
+    public class Forecast
+    {
+        [JsonProperty("dt_txt")]
+        public string DateText { get; set; }
+
+        [JsonProperty("main")]
+        public MainInfo Main { get; set; }
+
+        [JsonProperty("weather")]
+        public ForecastInfo[] Info { get; set; }
+    }
+
+    public class MainInfo
+    {
+        [JsonProperty("temp")]
+        public double Temperature { get; set; }
+
+        [JsonProperty("humidity")]
+        public int Humidity { get; set; }
+    }
+
+    public class ForecastInfo
+    {
+        [JsonProperty("icon")]
+        public string Icon { get; set; }
+    }
+    #endregion
+}
